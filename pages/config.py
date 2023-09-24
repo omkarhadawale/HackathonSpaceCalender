65267cdc73a267d577b6b87f1160f8fe74adabe6
omkarhadawale <omkarhadawale999@gmail.com>
2023-09-24 12:03:58 -0400

    Final

--- pages/config.py
+++
@@ -1,9 +0,0 @@
-URL = 'https://api.nasa.gov/planetary/apod'
-API_KEY = 'd3Hce5nIkb13gpFiEgivdbjXRQYTalZQB4e6OEr8'
-
-URL_NEWS='https://api.spaceflightnewsapi.net/v4/articles/'
-URL_BLOGS='https://api.spaceflightnewsapi.net/v4/blogs/' 
-
-CURIOSITY_INFO_URL = "https://api.nasa.gov/mars-photos/api/v1/manifests/Curiosity/?api_key=n5IwZZAaoFL7o0JDYv67vxiv8bl4QA7hzQidV1Ge"
-OPPORTUNITY_INFO_URL = "https://api.nasa.gov/mars-photos/api/v1/manifests/Opportunity/?api_key=n5IwZZAaoFL7o0JDYv67vxiv8bl4QA7hzQidV1Ge"
-SPIRIT_INFO_URL = "https://api.nasa.gov/mars-photos/api/v1/manifests/Spirit/?api_key=n5IwZZAaoFL7o0JDYv67vxiv8bl4QA7hzQidV1Ge"
